89c76aede7f708b41ce88ea3aa4222bc98ac6f8c
Zachary Balda <zacharybalda23@gmail.com>
2018-04-18 11:53:26 -0400

    wrote hypothesis and cost functions

--- logistic_regression.r
+++ logistic_regression.r
@@ -2,49 +2,20 @@
 #   set working directory in the console first
 #   e.g. setwd("/home/.../.../stats-35000-logistic-regression")
 
-# read in data
-diabetes_data=read.csv("./data/diabetes.csv")
-diabetes_filled_data=read.csv("./data/diabetes_filled.csv")
+# load data
+train_data=read.csv("./data/diabetes_filled_train.csv")
+test_data=read.csv("./data/diabetes_filled_test.csv")
 
-diabetes_filled_data$X = NULL
-
-# load in training data
-# load in testing data
-# remove number column(s)
-
-# split train and test datasets into two
-#   1. features X
-#   2. results Y
+# remove extra column
+train_data$X = NULL
+test_data$X = NULL
 
 # mean normalization?
 
-# define sigmoid function
-
-# define hypothesis functions
-#   take in n weights
-#   take in bias
-#   take in n feature values
-#   calculate dot product of weights and feature values
-#   add bias
-#   sigmoid
-#   return value
-
-# define cost functions
-#   take in n weights
-#   take in bias
-#   calculate cost from using train features and train results
-#   return cost
-
-# initialize k weights for k features
-# initialize bias
-
-# gradient descent
-#   take in alpha (learning rate)
-#   take in weights and bias
-#   compute gradient
-#   update weights and bias
-
-# get accuracy from test data
+# split features and outcomes
+X = as.matrix(train_data[,1:8])
+X = cbind(X, 1)
+Y = as.matrix(train_data$Outcome)
 
 # sigmoid function
 sigmoid = function(x) {
@@ -52,26 +23,42 @@ sigmoid = function(x) {
 }
 
 # linear hypothesis function
-linear_hyp = function() {
-  
+linear_hyp = function(weights, data) {
+  return(sigmoid(weights%*%data))
 }
 
 # quadratic hypothesis function
-quadratic_hyp = function() {
-  
+quadratic_hyp = function(weights, data) {
+  return(sigmoid(weights%*%data))
 }
 
 # mean square error cost function
-mean_squared_cost = function() {
-  
+mean_squared_cost = function(weights, data, hyp_funct) {
+  cost = (1/(2*nrow(data)))*sum((hyp_funct(weights, data) - Y)^2)
+  return(cost)
 }
 
 # logistic cost function
-logistic_cost = function() {
-  
+logistic_cost = function(weights, data, hyp_funct) {
+  cost = (1/nrow(data))*sum((-Y)*log(hyp_funct(weights, data)) - (1-Y)*log(1-hyp_funct(weights, data)))
+  return(cost)
 }
 
+
+# initialize vars for gradient descent
+weights = rep(0,ncol(X))
+learning_rate = 0.1
+epochs = 100
+
+logistic_cost(weights, train_data, quadratic_hyp)
+
 # gradient descent
-gradient_descent = function(cost_function, ) {
-  
+gradient_descent = function() {
+  # gradient descent
+  #   take in alpha (learning rate)
+  #   take in weights and bias
+  #   compute gradient
+  #   update weights and bias 
 }
+
+# get accuracy from test data
